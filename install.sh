4646831e58d36d7595c62ac60d3be8a259b18d30
Michael Williams <michael@dinosaur.io>
2012-06-06 14:39:05 -0700

    fixed a bug and added echo statements to install.sh.

--- install.sh
+++ install.sh
@@ -1,21 +1,26 @@
 #!/bin/sh
 
-# install sudo
+alias cd=pushd
+
+echo "installing sudo"
 aptitude install -y sudo
 
-# install git
+echo "installing git"
 aptitude install -y git
+echo "adding git user"
 adduser --system --shell $(which git-shell) --gecos 'git version control' --group --disabled-password --home /home/git git
 
-# make group user
 echo "making hackercoop user"
 adduser --shell /bin/bash --gecos 'hacker coop' --home /home/hackercoop hackercoop
+echo "generating ssh keys for hackercoop user"
 cd "/home/hackercoop/"
 sudo -H -u hackercoop mkdir .ssh
 sudo -H -u hackercoop ssh-keygen -t rsa -b 2048 -P "" -f ".ssh/id_rsa"
+echo "copying ssh keys for hackercoop user to git homedir"
 cp "/home/hackercoop/.ssh/id_rsa.pub" "/home/git/hackercoop.pub"
 
 # install gitolite
+echo "installing gitolite"
 cd "/home/git/"
 sudo -H -u git mkdir bin
 sudo -H -u git git clone git://github.com/sitaramc/gitolite
@@ -24,11 +29,12 @@ sudo -H -u git chown git:git "hackercoop.pub"
 sudo -H -u git bin/gitolite setup -pk "hackercoop.pub"
 
 # move repo to gitolite repos
-git clone --mirror $(dirname $0) "/home/git/repositories/$(basename $0).git"
-sudo -H -u git chown -R git:git "/home/git/repositories/$(basename $0).git"
+git clone --mirror $(dirname $0) "/home/git/repositories/$(basename $(dirname $0)).git"
+sudo -H -u git chown -R git:git "/home/git/repositories/$(basename $(dirname $0)).git"
 sudo -H -u git bin/gitolite setup
 
 # add repo to gitolite config
+echo "configuring gitolite"
 cd "/home/hackercoop"
 sudo -H -u hackercoop git clone git@localhost:gitolite-admin.git
 cd "gitolite-admin"
